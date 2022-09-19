665ab4f7873c8b61757bcbf3527bbd04181e756c
jawahendy <hendyn25@gmail.com>
2022-09-19 09:51:19 +0700

    Starter-Kit ReactNative

---
+++ StarterKit_ReactNative/App.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to webninar Celerates</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+  }
+})
