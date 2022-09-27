a57345a8174690980b1fcb373437c3782b7c9f9c
jawahendy <hendyn25@gmail.com>
2022-09-27 20:31:51 +0700

    final code

--- starterkit_flutter/lib/main.dart
+++ starterkit_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: non_constant_identifier_names, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -47,18 +48,112 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo.shade900,
       body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'Welcome To Webinar Celerates',
+        padding: EdgeInsets.only(left: 30, top: 30, right: 30),
+        child: Column(
+          children: [
+            SizedBox(
+              child: Card(
+                elevation: 20,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset('assets/slider1.jpg', fit: BoxFit.contain),
+                ),
               ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Title_Course(Title: "Popular Course"),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return SizedBox(
+                      height: 100,
+                      width: 100,
+                      child: Card(
+                        margin: EdgeInsets.all(10),
+                        elevation: 20,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.asset(ImageLogo[index],
+                              fit: BoxFit.contain),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Title_Course(Title: "Course"),
+            Expanded(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: ((context, index) {
+                    return Card(
+                      margin: EdgeInsets.only(right: 10, top: 10),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: ListTile(
+                        title: Text(titles[index]),
+                        subtitle: Text(subtitles[index]),
+                        leading: ClipRRect(
+                          borderRadius: BorderRadius.circular(8.0),
+                          child: Image.asset(ImageLogo[index],
+                              fit: BoxFit.contain),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class Title_Course extends StatelessWidget {
+  const Title_Course({
+    Key? key,
+    required this.Title,
+  }) : super(key: key);
+
+  final String Title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          Title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontFamily: 'Roboto',
+          ),
+        ),
+      ],
     );
   }
 }
